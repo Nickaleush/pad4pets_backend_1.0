a6fc226b741574086475abfdadcae613213950f1
Nickaleush <nickaleush@mail.ru>
2022-04-13 01:23:05 +0300

    Добавление питомца в базу данных, изменение данных о питомце

--- src/main/kotlin/pad4pets/entity/Pet.kt
+++ src/main/kotlin/pad4pets/entity/Pet.kt
@@ -4,7 +4,7 @@ import java.util.Date
 import javax.persistence.*
 
 @Entity
-@Table(name = "Pets")
+@Table(name = "pets")
 data class Pet(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
