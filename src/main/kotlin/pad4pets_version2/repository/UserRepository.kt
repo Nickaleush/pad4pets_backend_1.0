ebfdafd5f633b7095fc3cd3537dd428ef27df8ed
Nickaleush <nickaleush@mail.ru>
2022-04-19 22:39:23 +0300

    RebaseProject

--- src/main/kotlin/pad4pets_version2/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package pad4pets_version2.repository
-
-import pad4pets_version2.entity.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long> {
-
-}
